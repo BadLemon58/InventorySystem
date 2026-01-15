88fbc6fb20b9351585d2345255a3e59b0aab5473
Lemon <badlemon58@gmail.com>
2026-01-15 20:17:03 +0800

    small adjustment

--- InventorySystemManaement/ChangePassword.Designer.cs
+++ InventorySystemManaement/ChangePassword.Designer.cs
@@ -41,66 +41,73 @@
             // lblOldPass
             // 
             this.lblOldPass.AutoSize = true;
-            this.lblOldPass.Font = new System.Drawing.Font("Lucida Fax", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblOldPass.Location = new System.Drawing.Point(118, 95);
+            this.lblOldPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOldPass.Location = new System.Drawing.Point(96, 77);
+            this.lblOldPass.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblOldPass.Name = "lblOldPass";
-            this.lblOldPass.Size = new System.Drawing.Size(214, 23);
+            this.lblOldPass.Size = new System.Drawing.Size(153, 20);
             this.lblOldPass.TabIndex = 0;
             this.lblOldPass.Text = "Enter Old Password:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Lucida Fax", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(118, 147);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(89, 118);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(221, 23);
+            this.label2.Size = new System.Drawing.Size(160, 20);
             this.label2.TabIndex = 1;
             this.label2.Text = "Enter New Password:\r\n";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Lucida Fax", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(118, 198);
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(108, 161);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(202, 23);
+            this.label3.Size = new System.Drawing.Size(141, 20);
             this.label3.TabIndex = 2;
             this.label3.Text = "Confirm Password:";
             // 
             // txtboxOldPass
             // 
-            this.txtboxOldPass.Font = new System.Drawing.Font("Lucida Fax", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtboxOldPass.Location = new System.Drawing.Point(354, 87);
+            this.txtboxOldPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtboxOldPass.Location = new System.Drawing.Point(266, 71);
+            this.txtboxOldPass.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtboxOldPass.Name = "txtboxOldPass";
-            this.txtboxOldPass.Size = new System.Drawing.Size(218, 31);
+            this.txtboxOldPass.Size = new System.Drawing.Size(164, 26);
             this.txtboxOldPass.TabIndex = 3;
             this.txtboxOldPass.TextChanged += new System.EventHandler(this.txtboxOldPass_TextChanged);
             // 
             // txtboxNewPass
             // 
-            this.txtboxNewPass.Font = new System.Drawing.Font("Lucida Fax", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtboxNewPass.Location = new System.Drawing.Point(354, 141);
+            this.txtboxNewPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtboxNewPass.Location = new System.Drawing.Point(266, 115);
+            this.txtboxNewPass.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtboxNewPass.Name = "txtboxNewPass";
-            this.txtboxNewPass.Size = new System.Drawing.Size(218, 31);
+            this.txtboxNewPass.Size = new System.Drawing.Size(164, 26);
             this.txtboxNewPass.TabIndex = 4;
             this.txtboxNewPass.TextChanged += new System.EventHandler(this.txtboxNewPass_TextChanged);
             // 
             // txtboxConfirmPass
             // 
-            this.txtboxConfirmPass.Font = new System.Drawing.Font("Lucida Fax", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtboxConfirmPass.Location = new System.Drawing.Point(354, 195);
+            this.txtboxConfirmPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtboxConfirmPass.Location = new System.Drawing.Point(266, 158);
+            this.txtboxConfirmPass.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtboxConfirmPass.Name = "txtboxConfirmPass";
-            this.txtboxConfirmPass.Size = new System.Drawing.Size(218, 31);
+            this.txtboxConfirmPass.Size = new System.Drawing.Size(164, 26);
             this.txtboxConfirmPass.TabIndex = 5;
             this.txtboxConfirmPass.TextChanged += new System.EventHandler(this.txtboxConfirmPass_TextChanged);
             // 
             // btnChangePass
             // 
-            this.btnChangePass.Font = new System.Drawing.Font("Lucida Fax", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnChangePass.Location = new System.Drawing.Point(149, 307);
+            this.btnChangePass.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnChangePass.Location = new System.Drawing.Point(112, 249);
+            this.btnChangePass.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnChangePass.Name = "btnChangePass";
-            this.btnChangePass.Size = new System.Drawing.Size(183, 28);
+            this.btnChangePass.Size = new System.Drawing.Size(137, 23);
             this.btnChangePass.TabIndex = 6;
             this.btnChangePass.Text = "Change Password";
             this.btnChangePass.UseVisualStyleBackColor = true;
@@ -108,20 +115,21 @@
             // 
             // btnCancel
             // 
-            this.btnCancel.Font = new System.Drawing.Font("Lucida Fax", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCancel.Location = new System.Drawing.Point(389, 307);
+            this.btnCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancel.Location = new System.Drawing.Point(292, 249);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(183, 28);
+            this.btnCancel.Size = new System.Drawing.Size(137, 23);
             this.btnCancel.TabIndex = 7;
             this.btnCancel.Text = "Cancel";
             this.btnCancel.UseVisualStyleBackColor = true;
             // 
             // ChangePassword
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnChangePass);
             this.Controls.Add(this.txtboxConfirmPass);
@@ -130,6 +138,7 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.lblOldPass);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "ChangePassword";
             this.Text = "ChangePassword";
             this.ResumeLayout(false);
